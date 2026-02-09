13efae6b83e6cce5abba1b530941a968d36cdbdf
Likihes <likihes27.hp@gmail.com>
2026-02-09 17:44:34 +0530

    git new code for library

--- vars/gitCheckout.groovy
+++ vars/gitCheckout.groovy
@@ -1,13 +1,32 @@
-def call (Map config){
-    if (config.branch == 'master'){
-        checkout{[
-                    $class: 'GitSCM',
-                    branches: [[name: config.branch]],
-                    userRemoteConfigs: [[url: config.url,
-                    credentialsId: config.credentialsId]]
-   ]}
-}
-else {
-    error "Branch '${config.branch}' is not supported"
+def call(Map config) {
+
+    if (!config.branch || !config.url || !config.credentialsId) {
+        error "Missing required parameters: branch, url, credentialsId"
     }
+
+    echo "Checking out repo ${config.url}, branch ${config.branch}"
+
+    checkout([
+        $class: 'GitSCM',
+        branches: [[name: "*/${config.branch}"]],
+        userRemoteConfigs: [[
+            url: config.url,
+            credentialsId: config.credentialsId
+        ]]
+    ])
 }
+
+
+// def call (Map config){
+//     if (config.branch == 'master'){
+//         checkout{[
+//                     $class: 'GitSCM',
+//                     branches: [[name: config.branch]],
+//                     userRemoteConfigs: [[url: config.url,
+//                     credentialsId: config.credentialsId]]
+//    ]}
+// }
+// else {
+//     error "Branch '${config.branch}' is not supported"
+//     }
+// }
